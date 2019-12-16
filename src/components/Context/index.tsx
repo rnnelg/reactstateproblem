39a720cb2c0f8f1aa53d60c8502146737b7caf86
glenn@immiso.com <glenn@immiso.com>
2019-12-16 13:07:57 -0600

    adding state

--- src/components/Context/index.tsx
+++ src/components/Context/index.tsx
@@ -14,6 +14,33 @@ interface IActions {
 
 export class Provider extends Component {
 
+   /*  let question = new Question2();
+
+    question.setState(question.noAnswer);
+    console.log(`Order state: ${(question.getState()).constructor.name}`);
+    question.getState().AnswerQuestion();
+    question.getState().CheckAnswer(false);
+    console.log(`Order state: ${(question.getState()).constructor.name}`);
+    question.getState().CheckAnswer(true);
+    console.log(`Order state: ${(question.getState()).constructor.name}`);
+    question.getState().AnswerQuestion();
+    console.log(`Order state: ${(question.getState()).constructor.name}`);
+    question.getState().CheckAnswer(true);
+  
+    if(question.getState().constructor.name === "HasCorrectAnswerState") {
+      console.log("correct answer next question");
+    } else {
+      console.log("not the correct answer try again");
+    } */
+
+    question: Question2;
+
+    constructor(props: any){
+        super(props);
+        this.question = new Question2();
+        this.question.setState(this.question.noAnswer);
+    }
+
     state = {
         correctAnswer: null,
         buttonText: "Check"
@@ -23,6 +50,7 @@ export class Provider extends Component {
         this.setState({
             correctAnswer: answer
         });
+
     }
     
     handleCheckAnswer = () => {
@@ -63,5 +91,122 @@ export class Provider extends Component {
         );
     }
 }
+
+interface QuestionState{
+    question: Question2;
+  
+    AnswerQuestion(): void;
+    CheckAnswer(correctAnswer: boolean): void;
+  }
+  
+  class Question2{
+    public noAnswer: QuestionState;
+    public hasAnswer: QuestionState;
+    public hasCorrectAnswer: QuestionState;
+    public hasIncorrectAnswer: QuestionState;
+  
+    public currentQuestionState!: QuestionState;
+  
+    constructor(){
+      this.noAnswer = new NoAnswerState(this);
+      this.hasAnswer = new HasAnswerState(this);
+      this.hasCorrectAnswer = new HasCorrectAnswerState(this);
+      this.hasIncorrectAnswer = new HasIncorrectAnswerState(this);
+  
+      this.setState(this.noAnswer)
+    }
+  
+    public setState(questionState: QuestionState){
+      this.currentQuestionState = questionState;
+    }
+  
+    public getState(): QuestionState{
+      return this.currentQuestionState;
+    }
+  }
+  
+  class NoAnswerState implements QuestionState{
+      public question: Question2;
+      
+      constructor(question: Question2){
+        this.question = question;
+      }
+  
+      AnswerQuestion(): void {
+        this.question.setState(this.question.hasAnswer);
+        console.log(`Status: ${(this.question.getState()).constructor.name}`);
+      }
+  
+      CheckAnswer(correctAnswer: boolean): void {
+        this.question.setState(this.question.noAnswer);
+        console.log(`Status: ${(this.question.getState()).constructor.name}`);
+      }
+  }
+  
+  class HasAnswerState implements QuestionState{
+    public question: Question2;
+    
+    constructor(question: Question2){
+      this.question = question;
+    }
+  
+    AnswerQuestion(): void {
+      this.question.setState(this.question.hasAnswer);
+      console.log(`Status: ${(this.question.getState()).constructor.name}`);
+    }
+    CheckAnswer(correctAnswer: boolean): void {
+      if (correctAnswer){
+        this.question.setState(this.question.hasCorrectAnswer);
+      } else{
+        this.question.setState(this.question.hasIncorrectAnswer);
+      }
+      console.log(`Status: ${(this.question.getState()).constructor.name}`);
+    }
+  }
+  
+  class HasCorrectAnswerState implements QuestionState{
+    public question: Question2;
+    
+    constructor(question: Question2){
+      this.question = question;
+    }
+  
+    AnswerQuestion(): void {
+      this.question.setState(this.question.hasAnswer);
+      console.log(`Status: ${(this.question.getState()).constructor.name}`);
+    }
+  
+    CheckAnswer(correctAnswer: boolean): void {
+      if (correctAnswer){
+        this.question.setState(this.question.hasCorrectAnswer);
+      } else{
+        this.question.setState(this.question.hasIncorrectAnswer);
+      }
+      console.log(`Status: ${(this.question.getState()).constructor.name}`);
+    }
+  }
+  
+  class HasIncorrectAnswerState implements QuestionState{
+    public question: Question2;
+    
+    constructor(question: Question2){
+      this.question = question;
+    }
+  
+    AnswerQuestion(): void {
+      this.question.setState(this.question.hasAnswer);
+      console.log(`Status: ${(this.question.getState()).constructor.name}`);
+    }
+  
+    CheckAnswer(correctAnswer: boolean): void {
+      if (correctAnswer){
+        this.question.setState(this.question.hasCorrectAnswer);
+      } else{
+        this.question.setState(this.question.hasIncorrectAnswer);
+      }
+      console.log(`Status: ${(this.question.getState()).constructor.name}`);
+    }
+  }
+  
 
 export const Consumer = QuizContext.Consumer;
